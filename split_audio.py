9b84fae6eca5771d820a2bcdf4eca67c332320c4
hangpy <leehangbok2009@gmail.com>
2019-05-14 23:54:31 +0900

    copy and add tqdm module to pydub.silence.py, modified split_audio.py

--- split_audio.py
+++ split_audio.py
@@ -6,25 +6,37 @@ Created on Tue May  7 23:09:45 2019
 @author: chominjae
 """
 
-from pydub import AudioSegment 
-from pydub.silence import split_on_silence
+from pydub import AudioSegment
+# from pydub.silence import split_on_silence
+from util.silence import split_on_silence
 import os
 import argparse
+
 
 
 def split_operation(args):
 
     # audio file to split
     input_file = args.input
+    target_dir = os.path.join(os.path.abspath(''), 'assets', args.target)
     # output directory splitted audio files will be stored
     out_dir = os.path.join(os.path.abspath(''), 'assets', args.target, 'wavs')
 
-    audio_file = AudioSegment.from_file(args.input, format="mp3")
+    if not os.path.exists(target_dir):
+        os.mkdir(target_dir)
+
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+
+    audio_file = AudioSegment.from_file(input_file, format="mp3")
 
     # 48khz -> 16khz for google speech api usage
     audio_file = audio_file.set_frame_rate(16000)
     # multiple channel -> one channel for google speech api usage
     audio_file = audio_file.set_channels(1)
+
+    print('Split operation is started')
 
     audio_chunks = split_on_silence(audio_file,
                                     # must be silent for at least half a second
@@ -34,9 +46,10 @@ def split_operation(args):
                                     silence_thresh=-40, keep_silence=100
                                     )
 
-    print('Split operation is running...')
+
+
     for i, chunk in enumerate(audio_chunks):
-        out_file = os.path.join(out_dir, 'BN%02d_%04d' % args.target_version % i)
+        out_file = os.path.join(out_dir, 'BN%02d_%05d.wav' % (args.version, i))
         chunk.export(out_file, format="wav")
 
     print('Split operation is completed')
@@ -45,7 +58,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--input', required=True)
     parser.add_argument('--target', required=True)
-    parser.add_argument('--version', required=True)
+    parser.add_argument('--version', required=True, type=int, help='present version of audio batch file to split')
     args = parser.parse_args()
 
     # python split_audio.py --input [path for audio file to split] --target Benedict --version 1
