f74d4960c28a718587c4a884c23c3f9d9ac67a5e
hang <leehangbok2009@gmail.com>
2019-05-14 22:40:15 +0900

    modify split_audio.py file

--- split_audio.py
+++ split_audio.py
@@ -7,28 +7,51 @@ Created on Tue May  7 23:09:45 2019
 """
 
 from pydub import AudioSegment 
-from pydub.silence import split_on_silence 
+from pydub.silence import split_on_silence
+import os
+import argparse
 
 
+def split_operation(args):
 
-#AudioSegment.converter = '/Users/chominjae/anaconda3/lib/python3.7/site-packages/ffmpeg'
-#AudioSegment.ffprobe   = '/Users/chominjae/anaconda3/lib/python3.7/site-packages/ffprobe'
+    # audio file to split
+    input_file = args.input
+    # output directory splitted audio files will be stored
+    out_dir = os.path.join(os.path.abspath(''), 'assets', args.target, 'wavs')
 
-sound_file = AudioSegment.from_file("/Users/chominjae/Desktop/Capstone/Audio/Audio_original/Casanova/Casanova_5.mp3", format="mp3")
-#sound_file = AudioSegment.from_mp3('/Users/chominjae/Desktop/Capstone/Audio/Casanova_1.mp3') 
-audio_chunks = split_on_silence(sound_file, 
-    # must be silent for at least half a second 
-    min_silence_len=400, 
+    audio_file = AudioSegment.from_file(args.input, format="mp3")
 
-    # consider it silent if quieter than -16 dBFS 
-    silence_thresh=-40,keep_silence=100 
-) 
+    # 48khz -> 16khz for google speech api usage
+    audio_file = audio_file.set_frame_rate(16000)
+    # multiple channel -> one channel for google speech api usage
+    audio_file = audio_file.set_channels(1)
 
-for i, chunk in enumerate(audio_chunks): 
+    audio_chunks = split_on_silence(audio_file,
+                                    # must be silent for at least half a second
+                                    min_silence_len=400,
 
-    out_file = '/Users/chominjae/Desktop/Capstone/Audio/Audio_split/Casanova_5/Casanova_5_chunk{0}.wav'.format(i) 
-    #print("exporting", out_file)
-    chunk.export(out_file, format="wav")
-    
+                                    # consider it silent if quieter than -16 dBFS
+                                    silence_thresh=-40, keep_silence=100
+                                    )
 
-print("exporting complete")
+    print('Split operation is running...')
+    for i, chunk in enumerate(audio_chunks):
+        out_file = os.path.join(out_dir, 'BN%02d_%04d' % args.target_version % i)
+        chunk.export(out_file, format="wav")
+
+    print('Split operation is completed')
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input', required=True)
+    parser.add_argument('--target', required=True)
+    parser.add_argument('--version', required=True)
+    args = parser.parse_args()
+
+    # python split_audio.py --input [path for audio file to split] --target Benedict --version 1
+    # tacotron/assets/Benedict/wavs directory will be created
+    split_operation(args)
+
+
+if __name__ == '__main__':
+    main()
